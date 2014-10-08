1e0a1352534222c428708397ea8f6443d4756d43
dannydelott <dannydelott@gmail.com>
2014-10-08 13:45:22 -0700

    added haptic feedback to ActivityLevel1 touch events

--- app/src/main/java/level1to5/ActivityLevel1.java
+++ app/src/main/java/level1to5/ActivityLevel1.java
@@ -3,13 +3,14 @@ package level1to5;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.HapticFeedbackConstants;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.TextView;
 
 import broccoli.donotplaythisgame.R;
 
 
-public class ActivityLevel1 extends Activity implements View.OnLongClickListener, View.OnClickListener {
+public class ActivityLevel1 extends Activity implements View.OnLongClickListener, View.OnClickListener, View.OnTouchListener {
 
     TextView tvLevelIndicator;
 
@@ -20,20 +21,28 @@ public class ActivityLevel1 extends Activity implements View.OnLongClickListener
 
         tvLevelIndicator = (TextView) findViewById(R.id.tvLevelIndicator);
         tvLevelIndicator.setOnClickListener(this);
+        tvLevelIndicator.setOnTouchListener(this);
         tvLevelIndicator.setOnLongClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
-        view.performHapticFeedback(HapticFeedbackConstants.VIRTUAL_KEY,
-                HapticFeedbackConstants.FLAG_IGNORE_GLOBAL_SETTING);
+
     }
 
 
     @Override
     public boolean onLongClick(View view) {
-        view.performHapticFeedback(HapticFeedbackConstants.VIRTUAL_KEY,
-                HapticFeedbackConstants.FLAG_IGNORE_GLOBAL_SETTING);
+
+        return false;
+    }
+
+    @Override
+    public boolean onTouch(View view, MotionEvent motionEvent) {
+        if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
+            view.performHapticFeedback(HapticFeedbackConstants.VIRTUAL_KEY,
+                    HapticFeedbackConstants.FLAG_IGNORE_GLOBAL_SETTING);
+        }
         return false;
     }
 }
